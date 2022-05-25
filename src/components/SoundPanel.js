ed9a5ae1b8fd2c2fe1ab8ee01f231823e7621ece
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-25 03:19:06 -0400

    restyle sound panel

--- src/components/SoundPanel.js
+++ src/components/SoundPanel.js
@@ -6,28 +6,31 @@ import StringSound from './StringSound';
 export default function SoundPanel() {
   return (
     <section className="dashboard__panel relative border bg-meringue">
-      <div className="flex flex-col h-full">
-        <div className="flex">
+      <div className="flex h-full">
+        <div className="w-1/2 h-full flex flex-col items-center justify-between">
           <div className="w-1/2">
             <h1 className="mt-3 mb-2 font-display text-teal text-4xl text-center">
               Sounds
             </h1>
           </div>
-          <div className="w-1/2">
-            <h1 className="mt-3 mb-2 font-display text-teal text-4xl text-center">
-              Timer
-            </h1>
-          </div>
-        </div>
 
-        <div className="flex items-center mt-6">
-          <div className="w-1/2">
+          <div className="w-7/12 h-1/3 mx-auto px-6 py-2 flex flex-col justify-around text-meringue bg-teal border-2 rounded-xl">
             <StringSound />
             <RainSound />
             <PianoSound />
           </div>
 
-          <div className="w-1/2 border-l-2 border-dashed">
+          <div></div>
+        </div>
+
+        <div className="w-1/2 flex flex-col items-center">
+          <div className="w-1/2">
+            <h1 className="mt-3 mb-2 font-display text-teal text-4xl text-center">
+              Timer
+            </h1>
+          </div>
+
+          <div className="flex items-center h-full py-8">
             <PomodoroTimer />
           </div>
         </div>
