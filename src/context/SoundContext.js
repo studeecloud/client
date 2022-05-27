4055dc9bdb4641f80af985b49ce3c5455e4d4f44
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-26 20:42:38 -0400

    refactor to work with a single Sound component

--- src/context/SoundContext.js
+++ src/context/SoundContext.js
@@ -1,27 +1,26 @@
-import { createContext, useState, useEffect, useContext } from 'react';
+import { createContext, useState, useContext } from 'react';
 import ReactHowler from 'react-howler';
 
 const SoundContext = createContext();
 
 function SoundProvider({ children }) {
-  //Determines if each track is playing, set through function located in each sound component
-  const [piano, setPiano] = useState(false);
-  const [rain, setRain] = useState(false);
-  const [string, setString] = useState(false);
-
+  // Determines if ANY track is playing
+  const [playing, setPlaying] = useState(false);
   const [src, setSrc] = useState('');
 
-  //React-Howler state managing playing status of tracks
-  const [playing, setPlaying] = useState(false);
-
-  //HELPER FUNCTION: COnnect playingState to each individaul sound's state
-  //Setplaying is called when play-button of any sound component is clicked
-  useEffect(() => {
-    setPlaying(piano || rain || string ? true : false);
-  }, [piano, rain, string]);
+  const toggleSound = (fileName) => {
+    if (playing) {
+      setSrc('');
+      setPlaying(false);
+      return;
+    }
+    setSrc(`https://studeecloud-server.herokuapp.com/public/${fileName}`);
+    setPlaying(true);
+  };
 
   //All necessary states for app to track when rerendering
-  const states = { piano, setPiano, rain, setRain, string, setString, setSrc };
+  const states = { toggleSound, playing };
+
   return (
     <SoundContext.Provider value={states}>
       {children}
