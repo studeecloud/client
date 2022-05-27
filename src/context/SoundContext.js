c31d2705b53da29e12fc4647f52d90260f10483b
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-27 04:02:08 -0400

    Add comments for toggleSound()

--- src/context/SoundContext.js
+++ src/context/SoundContext.js
@@ -6,9 +6,12 @@ const SoundContext = createContext();
 function SoundProvider({ children }) {
   const [src, setSrc] = useState('');
 
+  // If a sound is playing, we pause it; if no sound is playing, we begin playing the currently selected sound
   const toggleSound = (fileName) => {
+    // When a sound is playing, 'src' is a String reflecting the file name of the sound being played
+    // When a sound is not playing, 'src' is an empty String
     setSrc(
-      !!src ? '' : `https://studeecloud-server.herokuapp.com/public/${fileName}`
+      src ? '' : `https://studeecloud-server.herokuapp.com/public/${fileName}`
     );
   };
 
