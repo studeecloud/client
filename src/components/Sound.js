02516c17c6b8dc7bba52d7f25c29809183f64e42
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-26 20:42:56 -0400

    complete Sound component implementation

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -3,17 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function Sound({ fileName, soundName }) {
-  const { sound, setSound, setSrc } = useSound();
-
-  //Sets playing state to true through HELPER FUNCTION in SoundContext.js
-  const playSound = () => {
-    setSound(true);
-    setSrc(`https://studeecloud-server.herokuapp.com/public/${fileName}`);
-  };
-
-  const pauseSound = () => {
-    setSound(false);
-  };
+  const { toggleSound, playing } = useSound();
 
   return (
     <section className="flex flex-row justify-between items-center">
@@ -21,12 +11,18 @@ export default function Sound({ fileName, soundName }) {
         <h1 className="font-body text-2xl text-center">{soundName}</h1>
       </div>
       <div>
-        {sound ? (
-          <button onClick={pauseSound} className="align-middle">
+        {playing ? (
+          <button
+            onClick={() => toggleSound(fileName)}
+            className="align-middle"
+          >
             <FontAwesomeIcon icon={solid('circle-pause')} className="h-5" />
           </button>
         ) : (
-          <button onClick={playSound} className="align-middle">
+          <button
+            onClick={() => toggleSound(fileName)}
+            className="align-middle"
+          >
             <FontAwesomeIcon icon={solid('circle-play')} className="h-5" />
           </button>
         )}
