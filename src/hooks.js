68b24dd6d7c89d7dc3917109128b0d0c2d22519e
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-27 02:21:20 -0400

    refactor useInterval into hooks.js

---
+++ src/hooks.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+// Custom hook useInterval will take an action on a set time interval
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+export { useInterval };
