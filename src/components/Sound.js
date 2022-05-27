b4aad938e37c5f19e90d77b3bad4699b0a95113b
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-26 22:14:39 -0400

    add conditional classes to identify selected sound

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -1,22 +1,27 @@
-import { useSound } from '../context/SoundContext';
+import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { useSound } from '../context/SoundContext';
 
-export default function Sound({ file, name }) {
+export default function Sound({ file, name, selectSound, isSelected }) {
   const { toggleSound, playing } = useSound();
+
+  const soundNameClasses = classnames('font-body text-2xl text-center', {
+    'text-coral': isSelected,
+  });
 
   return (
     <section className="flex flex-row justify-between items-center">
       <div>
-        <h1 className="font-body text-2xl text-center">{name}</h1>
+        <h1 className={soundNameClasses}>{name}</h1>
       </div>
       <div>
         {playing ? (
-          <button onClick={() => toggleSound(file)} className="align-middle">
+          <button onClick={selectSound} className="align-middle">
             <FontAwesomeIcon icon={solid('circle-pause')} className="h-5" />
           </button>
         ) : (
-          <button onClick={() => toggleSound(file)} className="align-middle">
+          <button onClick={selectSound} className="align-middle">
             <FontAwesomeIcon icon={solid('circle-play')} className="h-5" />
           </button>
         )}
