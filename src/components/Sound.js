2aa7a23e6b308272ea90778ff6c9f1ea107dc63b
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-26 20:54:55 -0400

    rename variables for clarity

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -2,27 +2,21 @@ import { useSound } from '../context/SoundContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
-export default function Sound({ fileName, soundName }) {
+export default function Sound({ file, name }) {
   const { toggleSound, playing } = useSound();
 
   return (
     <section className="flex flex-row justify-between items-center">
       <div>
-        <h1 className="font-body text-2xl text-center">{soundName}</h1>
+        <h1 className="font-body text-2xl text-center">{name}</h1>
       </div>
       <div>
         {playing ? (
-          <button
-            onClick={() => toggleSound(fileName)}
-            className="align-middle"
-          >
+          <button onClick={() => toggleSound(file)} className="align-middle">
             <FontAwesomeIcon icon={solid('circle-pause')} className="h-5" />
           </button>
         ) : (
-          <button
-            onClick={() => toggleSound(fileName)}
-            className="align-middle"
-          >
+          <button onClick={() => toggleSound(file)} className="align-middle">
             <FontAwesomeIcon icon={solid('circle-play')} className="h-5" />
           </button>
         )}
