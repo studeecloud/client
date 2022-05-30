2991fba2495003b010968b3214f201273d5b5ce8
Lisa Meyer <lisamermey@gmail.com>
2022-05-30 15:13:45 -0400

    Import BrowserRouter, Routes, Route to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import Loader from './Loader';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Loader />);
+root.render(
+  <Loader />
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
